86da2eb3bc23fca05545611e6193947e58f7cb04
Josh Espinas <joshua.espinas@gmail.com>
2021-08-08 20:41:55 +1200

    connects to a front end (#1)

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var path = require('path')
+
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + ''));
+
 app.get('/', function (req, res) {
-  res.send('Hello World! I am a node app.');
+  res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 app.listen(port, function () {
